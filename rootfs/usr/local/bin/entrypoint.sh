fdb969d950652a17ace3d2e4f1d11a9888c5a956
Jeffrey Böhm <jeffrey.boehm@twt.de>
2016-06-20 00:43:54 +0200

    Initial commit

---
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+wait-mysql.sh
+
+if ! [ -r /var/www/html/config.php ]
+then
+	echo "Installing Shopware..."
+
+	tar --strip 1 -zxf /root/shopware.tar.gz
+	ln -s /usr/local/bin/composer composer.phar
+
+	ant -f build/build.xml \
+		-Ddb.user=${MYSQL_USER} \
+		-Ddb.password=${MYSQL_PASSWORD} \
+		-Ddb.name=${MYSQL_DATABASE} \
+		-Ddb.host=${MYSQL_HOST} \
+		build-unit
+fi
+
+if [ -z $@ ]
+then
+	echo "Starting Webserver.."
+	chmod 777 var/cache/ var/log/
+	apache2-foreground
+else
+	case "$@" in
+		cc)
+    		var/cache/clear_cache.sh
+	    	;;
+		sh)
+			bash
+		    ;;
+		*)
+			echo "Commands: cc, grunt, sh"
+			;;
+esac
+fi
---
+++ rootfs/usr/local/bin/wait-mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Waiting for MySQL"
+
+until mysql -h "${MYSQL_HOST}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SHOW DATABASES;" &> /dev/null
+do
+	echo -n "."
+	sleep 1
+done
+
+echo "MySQL ready!"
