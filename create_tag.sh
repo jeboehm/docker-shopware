948d58d7d1aae6607ba1942176bd8a719260046c
Jeff Boehm <jeffrey.boehm@surplex.com>
2018-06-13 15:37:37 +0200

    Helper script to create new versions

---
+++ create_tag.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+get_latest() {
+    echo $(basename `find tags -type d -mindepth 1 | sort --version-sort | tail -n 1`)
+}
+
+TARGET_VERSION=${1}
+LATEST_VERSION=$(get_latest)
+
+if ! [ "${TARGET_VERSION}" ]
+then
+    echo "Target version missing"
+    exit 1
+fi
+
+if [ -d "tags/${TARGET_VERSION}" ]
+then
+    echo "Version ${TARGET_VERSION} already exists."
+    exit 1
+fi
+
+cp -a tags/${LATEST_VERSION} tags/${TARGET_VERSION}
+sed -i.bak s:${LATEST_VERSION}:${TARGET_VERSION}:g docker-compose.yml tags/${TARGET_VERSION}/Dockerfile
